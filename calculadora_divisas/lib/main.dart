da852254be0564082a51089be4493c1debfa885e
JAMITH ALEXANDER GARCÍA ARRIETA <87336025+djkdevise@users.noreply.github.com>
2022-11-05 11:52:40 -0500

    fontend_jamith
    Creación de variables globales,creaciosn de clases, creacion de objetos tipo listas, creacion de widgets, creacion de textbox

--- calculadora_divisas/lib/main.dart
+++ calculadora_divisas/lib/main.dart
@@ -7,24 +7,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Calculadora de Divisas',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Calculadora Home Page'),
     );
   }
 }
@@ -32,84 +22,226 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+final textOrigen = TextEditingController();
+final textDestino = TextEditingController();
+
+String op1 = 'USD';
+String op2 = 'COP';
+List<DropdownMenuItem<String>> listaMonedas = [
+  DropdownMenuItem(
+    child: Text('USD'),
+    value: 'USD',
+  ),
+  DropdownMenuItem(
+    child: Text('COP'),
+    value: 'COP',
+  ),
+  DropdownMenuItem(
+    child: Text('EUR'),
+    value: 'EUR',
+  ),
+  DropdownMenuItem(
+    child: Text('GBP'),
+    value: 'GBP',
+  ),
+  DropdownMenuItem(
+    child: Text('MXN'),
+    value: 'MXN',
+  ),
+  DropdownMenuItem(
+    child: Text('BRL'),
+    value: 'BRL',
+  ),
+  DropdownMenuItem(
+    child: Text('ARS'),
+    value: 'ARS',
+  ),
+  DropdownMenuItem(
+    child: Text('CLP'),
+    value: 'CLP',
+  ),
+  DropdownMenuItem(
+    child: Text('PEN'),
+    value: 'PEN',
+  ),
+  DropdownMenuItem(
+    child: Text('UYU'),
+    value: 'UYU',
+  ),
+  DropdownMenuItem(
+    child: Text('CRC'),
+    value: 'CRC',
+  ),
+  DropdownMenuItem(
+    child: Text('CUP'),
+    value: 'CUP',
+  ),
+  DropdownMenuItem(
+    child: Text('GTQ'),
+    value: 'GTQ',
+  ),
+  DropdownMenuItem(
+    child: Text('HNL'),
+    value: 'HNL',
+  ),
+  DropdownMenuItem(
+    child: Text('NIO'),
+    value: 'NIO',
+  ),
+  DropdownMenuItem(
+    child: Text('SVC'),
+    value: 'SVC',
+  ),
+  DropdownMenuItem(
+    child: Text('PYG'),
+    value: 'PYG',
+  ),
+  DropdownMenuItem(
+    child: Text('BOB'),
+    value: 'BOB',
+  ),
+  DropdownMenuItem(
+    child: Text('DOP'),
+    value: 'DOP',
+  ),
+  DropdownMenuItem(
+    child: Text('VEF'),
+    value: 'VEF',
+  ),
+  DropdownMenuItem(
+    child: Text('PAB'),
+    value: 'PAB',
+  ),
+  DropdownMenuItem(
+    child: Text('BGN'),
+    value: 'BGN',
+  ),
+  DropdownMenuItem(
+    child: Text('CZK'),
+    value: 'CZK',
+  ),
+  DropdownMenuItem(
+    child: Text('DKK'),
+    value: 'DKK',
+  ),
+  DropdownMenuItem(
+    child: Text('HUF'),
+    value: 'HUF',
+  ),
+];
+
+class calculadora {
+  final titulo;
+  final color;
+  final icono;
+
+  calculadora(this.titulo, this.color, this.icono);
+}
+
+List<calculadora> cal = [
+  calculadora("9", Colors.lightBlue, Icon(Icons.abc)),
+  calculadora("8", Colors.lightBlue, Icon(Icons.abc)),
+  calculadora("7", Colors.lightBlue, Icon(Icons.abc)),
+  calculadora("6", Colors.lightBlue, Icon(Icons.abc)),
+  calculadora("5", Colors.lightBlue, Icon(Icons.abc)),
+  calculadora("4", Colors.lightBlue, Icon(Icons.abc)),
+  calculadora("3", Colors.lightBlue, Icon(Icons.abc)),
+  calculadora("2", Colors.lightBlue, Icon(Icons.abc)),
+  calculadora("1", Colors.lightBlue, Icon(Icons.abc)),
+  calculadora("0", Colors.lightBlue, Icon(Icons.abc)),
+  calculadora("Limpiar", Colors.lightBlue, Icon(Icons.cleaning_services)),
+  calculadora("=", Colors.lightBlue, Icon(Icons.abc)),
+];
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Padding(
+          padding: EdgeInsets.all(25),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Text("Moneda origen: "),
+                  DropdownButton(
+                      items: listaMonedas,
+                      value: op1,
+                      onChanged: (value) {
+                        setState(() {
+                          op1 = value.toString();
+                        });
+                      }),
+                  Text("Moneda destino: "),
+                  DropdownButton(
+                      items: listaMonedas,
+                      value: op2,
+                      onChanged: (value) {
+                        setState(() {
+                          op2 = value.toString();
+                        });
+                      }),
+                ],
+              ),
+              TextField(
+                controller: textOrigen,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Ingrese el monto',
+                  icon: Icon(Icons.monetization_on),
+                ),
+              ),
+              TextField(
+                controller: textDestino,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Ingrese el monto',
+                  icon: Icon(Icons.monetization_on),
+                ),
+              ),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                  ),
+                  itemCount: cal.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      color: cal[index].color,
+                      child: Center(
+                        child: index == 10
+                            ? cal[index].icono
+                            : Text(
+                                cal[index].titulo,
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
